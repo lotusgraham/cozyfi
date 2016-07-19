2047cfe0f2c7881b141bb74a0cbed51fa647ef5a
Elias Mason <eliasjmason@gmail.com>
2016-07-18 23:26:14 -0700

    Creates basic server file with post route for a workspace.

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.use(express.static('public'));
+
+app.post('/workspaces', (req, res) => {
+    res.json(req.body);
+    //TODO: Process real workspace body
+});
+
+app.listen(8081, function() {
+    console.log('Running on port 8081');
+});
+
+exports.app = app;
