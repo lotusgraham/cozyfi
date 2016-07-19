52ffcf28510603b6a870db0526ea20f22faaae5f
Elias Mason <eliasjmason@gmail.com>
2016-07-19 13:51:55 -0700

    Removes Firebase requires from index.jsx.

--- js/index.jsx
+++ js/index.jsx
@@ -19,17 +19,6 @@ import { Router, Route, Link, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from '../redux/store';
 import Form from './Form'
-import firebase from "firebase/app";
-require("firebase/auth");
-require("firebase/database");
-
-var config = {
-  apiKey: "AIzaSyBRPTZj2zc0cP01-4icR9TEma70maoNF1I",
-  authDomain: "cozyfi-963e1.firebaseapp.com",
-  databaseURL: "https://cozyfi-963e1.firebaseio.com",
-  storageBucket: "",
-};
-firebase.initializeApp(config);
 
 const NavBar = () => (
   <AppBar
