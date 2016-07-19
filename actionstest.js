1eba80b3e6a8416e51a91518e7c988e0177730bd
Elias Mason <eliasjmason@gmail.com>
2016-07-19 13:31:59 -0700

    A file for attempting to test our actions.

--- actionstest.js
+++ actionstest.js
@@ -1,2 +1,11 @@
-require('./redux/store.js');
-console.log(store);
+var addWorkspaceSuccess = require('./redux/actions/workspace.js').addWorkspaceSuccess;
+var addWorkspace = require('./redux/actions/workspace.js').addWorkspace;
+var reducer = require('./redux/reducers/workspace.js')
+var store = require('./redux/store.js');
+
+let testContent = {
+    "name": "A Place",
+    "desc": "A desc"
+};
+
+store.dispatch(addWorkspace(testContent))
