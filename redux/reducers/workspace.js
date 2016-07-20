6cb809847c0905e1eacd4819a13725bce4619bfd
Elias Mason <eliasjmason@gmail.com>
2016-07-19 23:36:22 -0700

    Reducer now handles the successful addWorkspaceSuccess action. Bug: individual props return 'undefined' but workspace object is pushed successfully?

--- redux/reducers/workspace.js
+++ redux/reducers/workspace.js
@@ -19,24 +19,22 @@ const initialState = {
 const workspaceReducer = (state, action) => {
 	state = state || initialState;
     if (action.type === 'ADD_WORKSPACE_SUCCESS') {
+        console.log (action.workspace);
         let newState = update(state, {
-            $push: {
-                currentWorkspaces: action.workspace
-            },
-            $set: {
-                workspaceSaved: true,
-                GooglePlaces: action.workspace.GooglePlaces,
-                hasFastWifi: action.workspace.hasWifi,
-                hasCaffeine: action.workspace.hasCaffeine,
-                hasTableSpace: action.workspace.hasTableSpace,
-            	hasOutdoorSpace: action.workspace.hasOutdoorSpace,
-                isQuiet: action.workspace.isQuiet,
-                quirks: action.workspace.quirks,
-                perks: action.workspace.perks,
-                directions: action.workspace.directions
-            }
+            currentWorkspaces: { $push: [action.workspace]},
+            workspaceSaved: { $set:true },
+            GooglePlaces: { $set:action.workspace.Google_Places },
+            hasFastWifi: { $set:action.workspace.hasWifi },
+            hasCaffeine: { $set:action.workspace.hasCaffeine} ,
+            hasTableSpace: { $set:action.workspace.hasTableSpace },
+            hasOutdoorSpace: { $set:action.workspace.hasOutdoorSpace },
+            isQuiet: { $set:action.workspace.isQuiet},
+            quirks: { $set:action.workspace.quirks },
+            perks: { $set:action.workspace.perks },
+            directions: { $set:action.workspace.directions }
         });
         state = newState;
+        console.log(state);
     }
     return state;
 };
