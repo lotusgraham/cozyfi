d4c301da5606faaa6964bc0ecc55658552320c43
Elias Mason <eliasjmason@gmail.com>
2016-07-19 22:27:38 -0700

    Workspace reducer & initial state  now reflect the form for submitting a workspace.

--- redux/reducers/workspace.js
+++ redux/reducers/workspace.js
@@ -5,25 +5,35 @@ var update = require('react-addons-update');
 const initialState = {
 	currentWorkspaces: [],
 	workspaceSaved: false,
-	workspaceTitle: null,
-	workspaceDesc: null
-}
+    name: null,
+    hasFastWifi: null,
+    hasCaffeine: null,
+    hasTableSpace: null,
+	hasOutdoorSpace: null,
+    isQuiet: null,
+    quirks: null,
+    perks: null,
+    directions: null
+};
 
 const workspaceReducer = (state, action) => {
 	state = state || initialState;
-	if (action.type === 'ADD_WORKSPACE_SUCCESS') {
-		let newState = update(state, {
-			workspaceSaved: {
-				$set: true
-			}
-		});
-        state = newState;
-	}
-    if (action.type === 'STORE_WORKSPACE') {
+    if (action.type === 'ADD_WORKSPACE_SUCCESS') {
         let newState = update(state, {
+            $push: {
+                currentWorkspaces: action.workspace
+            },
             $set: {
-                workspaceTitle: action.workspaceTitle,
-                workspaceDesc: action.workspaceDesc
+                workspaceSaved: true,
+                name: action.workspace.name,
+                hasFastWifi: action.workspace.hasWifi,
+                hasCaffeine: action.workspace.hasCaffeine,
+                hasTableSpace: action.workspace.hasTableSpace,
+            	hasOutdoorSpace: action.workspace.hasOutdoorSpace,
+                isQuiet: action.workspace.isQuiet,
+                quirks: action.workspace.quirks,
+                perks: action.workspace.perks,
+                directions: action.workspace.directions
             }
         });
         state = newState;
