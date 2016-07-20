268e62d5110c4e8bab5bf585ef1f41e5fc9b162a
Elias Mason <eliasjmason@gmail.com>
2016-07-19 23:34:28 -0700

    addWorkspaceSuccess now dispatches with the supplied workspace.

--- redux/actions/workspace.js
+++ redux/actions/workspace.js
@@ -3,28 +3,28 @@ var firebaseApp = require('../../js/Firebase.jsx');
 const addWorkspaceSuccess = (workspace) => {
 	return {
 		type: 'ADD_WORKSPACE_SUCCESS',
-        workspace: workspace
+		workspace: workspace
 	};
 };
 
 function addWorkspace(workspace) {
 	return function(dispatch) {
-        let workspacesRef = firebaseApp.ref('/workspaces/');
-        let testContent = {
-    "Google Places": "AvalancheCafe",
-    "hasWifi": false,
-    "hasCaffeine": false,
-    "hasFood": false,
-    "hasOutlets": true,
-    "hasTableSpace": true,
-    "hasOutdoorSpace": false,
-    "isQuiet": false,
-    "quirks": "quirky",
-    "perks": "perky",
-    "directions": "directy"
-}
-			dispatch(addWorkspaceSuccess(testContent));
-            workspacesRef.push(testContent);
+		let workspacesRef = firebaseApp.ref('/workspaces/');
+		let testContent = {
+			"Google Places": "AvalancheCafe",
+			"hasWifi": false,
+			"hasCaffeine": false,
+			"hasFood": false,
+			"hasOutlets": true,
+			"hasTableSpace": true,
+			"hasOutdoorSpace": false,
+			"isQuiet": false,
+			"quirks": "quirky",
+			"perks": "perky",
+			"directions": "directy"
+		}
+		dispatch(addWorkspaceSuccess(workspace));
+		workspacesRef.push(workspace);
 
 	}
 }
