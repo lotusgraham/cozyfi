aa07db0f859f7aa750950f036ca962b42f4c186c
Elias Mason <eliasjmason@gmail.com>
2016-07-21 09:22:06 -0700

    Cleanup. Removes unnecessary individual props from the definition of the initialState & the reducer.

--- redux/reducers/workspace.js
+++ redux/reducers/workspace.js
@@ -4,49 +4,25 @@ var update = require('react-addons-update');
 var setCurrentPlace = require('../actions/workspace').setCurrentPlace;
 
 const initialState = {
-    description: null,
-	currentPlace: null,
 	currentWorkspaces: [],
 	workspaceSaved: false,
-    GooglePlaces: null,
-    hasFastWifi: null,
-    hasCaffeine: null,
-    hasTableSpace: null,
-	hasOutdoorSpace: null,
-    isQuiet: null,
-    isAccessible: null,
-    quirks: null,
-    perks: null,
-    directions: null
 };
 
 const workspaceReducer = (state, action) => {
 	state = state || initialState;
     if (action.type === 'ADD_WORKSPACE_SUCCESS') {
-        console.log (action.workspace);
         let newState = update(state, {
-            description: { $set:action.workspace.description },
             currentWorkspaces: { $push: [action.workspace]},
             workspaceSaved: { $set:true },
-            hasFastWifi: { $set:action.workspace.hasWifi },
-            hasCaffeine: { $set:action.workspace.hasCaffeine} ,
-            hasTableSpace: { $set:action.workspace.hasTableSpace },
-            hasOutdoorSpace: { $set:action.workspace.hasOutdoorSpace },
-            isQuiet: { $set:action.workspace.isQuiet},
-            isAccessible: { $set:action.workspace.isAccessible},
-            quirks: { $set:action.workspace.quirks },
-            perks: { $set:action.workspace.perks },
-            directions: { $set:action.workspace.directions }
-
         });
         state = newState;
+        console.log(state);
 	}
 	if (action.type === 'SET_CURRENT_PLACE') {
 		let newState = update(state, {
 		currentPlace: {$set: action.place}
 		})
 		state = newState;
-		console.log(state);
 	}
     return state;
 };
