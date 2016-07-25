13c595d2b98b78323fb57c62cbf7284e690d7052
Elias Mason <eliasjmason@gmail.com>
2016-07-25 14:05:26 -0700

    Reorganizes actions.

--- redux/actions/workspace.js
+++ redux/actions/workspace.js
@@ -1,64 +1,13 @@
 var firebaseApp = require('../../js/Firebase.jsx');
 require('isomorphic-fetch');
 
-const fetchMapData = (workspaces) => {
-    return (dispatch) => {
-        // nest this in a loop; perform this work on array
-        let workspace = {
-            "placeId":"ChIJ9-25gKYsDogRWBT2lu-OSi0",
-            "description": "short description",
-            "hasWifi": false,
-            "hasCaffeine": false,
-            "hasFood": false,
-            "hasOutlets": true,
-            "hasTableSpace": true,
-            "hasOutdoorSpace": false,
-            "isQuiet": false,
-            "isAccessible": false,
-            "quirks": "quirky",
-            "perks": "perky",
-            "directions": "directy"
-        }
-            let gMapsBaseUrl = 'https://maps.googleapis.com/maps/api/place/details/json',
-                query = {
-                placeid:  workspace.placeId,
-                key: 'AIzaSyDEW1grx0AbwSozmAu0fi7HczQn6D0UFlQ'
-            },
-                params = Object.keys(query)
-                    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(query[key]))
-                    .join("&")
-                    .replace(/%20/g, "+");
-            fetch(gMapsBaseUrl + '?' + params)
-                .then(res => res.json())
-                .then(res => {
-                    let googleData = res.result;
-                    let workspaceWithGData =
-                      Object.assign({}, workspace, {googleData: googleData});
-                      return console.log(workspaceWithGData);
-                });
-    }
-}
-
-const fetchMapDataSuccess = (workspaces) => {
-    return {
-        type: 'FETCH_MAP_DATA_SUCCESS',
-        place
-    }
-}
-
-const saveMapPlaceSuccess = (place) => {
-    return {
-        type: 'SAVE_MAP_PLACE_SUCCESS',
-        place
-    }
-}
-
 const setCurrentPlace = (placeId) => {
     return {
         type: 'SET_CURRENT_PLACE',
         placeId
     }
 }
+
 
 const fetchWorkspaceData = (filterParams) => {
     return (dispatch) => {
@@ -79,6 +28,47 @@ const fetchWorkspaceDataSuccess = (workspaces) => {
     }
 }
 
+const fetchMapData = (workspaces) => {
+    return (dispatch) => {
+        // nest this in a loop; perform this work on array
+        let workspace = {
+            "placeId":"ChIJ9-25gKYsDogRWBT2lu-OSi0",
+            "description": "short description",
+            "hasWifi": false,
+            "hasCaffeine": false,
+            "hasFood": false,
+            "hasOutlets": true,
+            "hasTableSpace": true,
+            "hasOutdoorSpace": false,
+            "isQuiet": false,
+            "isAccessible": false,
+            "quirks": "quirky",
+            "perks": "perky",
+            "directions": "directy"
+        }
+        let gMapsBaseUrl = 'https://maps.googleapis.com/maps/api/place/details/json',
+        query = {
+            placeid:  workspace.placeId,
+            key: 'AIzaSyDEW1grx0AbwSozmAu0fi7HczQn6D0UFlQ'
+        },
+        params = Object.keys(query)
+        .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(query[key]))
+        .join("&")
+        .replace(/%20/g, "+");
+        fetch(gMapsBaseUrl + '?' + params)
+        .then(res => res.json())
+        .then(res => {
+            let workspacesWithGData =
+            Object.assign({}, workspace, {googleData: res.result});
+            return dispatch(fetchMapDataSuccess(workspacesWithGData));
+        });
+    }
+}
+
+const fetchMapDataSuccess = (workspaces) => {
+    return dispatch(updateWorkspaceCache)
+}
+
 const addWorkspace = (workspace) => {
     return function(dispatch) {
         let workspacesRef = firebaseApp.ref('/workspaces/');
@@ -96,8 +86,6 @@ const addWorkspaceSuccess = (workspace) => {
 };
 
 
-
-
 const removeWorkspace = (workspaceId, workspaceIndex) => {
     // assumes index of array of current workspaces can be captured at dispatch time.
     return function(dispatch) {
@@ -105,7 +93,6 @@ const removeWorkspace = (workspaceId, workspaceIndex) => {
         workspaceRef.remove().then(() => {
             dispatch(removeWorkspaceSuccess(workspaceIndex));
         })
-
     }
 };
 
@@ -116,13 +103,13 @@ const removeWorkspaceSuccess = (index) => {
     }
 };
 
-exports.fetchMapData = fetchMapData;
-exports.fetchMapDatasSuccess = fetchMapDataSuccess;
-
 exports.setCurrentPlace = setCurrentPlace;
 
 exports.fetchWorkspaceData = fetchWorkspaceData;
 exports.fetchWorkspaceDataSuccess = fetchWorkspaceDataSuccess;
+
+exports.fetchMapData = fetchMapData;
+exports.fetchMapDatasSuccess = fetchMapDataSuccess;
 
 exports.addWorkspace = addWorkspace;
 exports.addWorkspaceSuccess = addWorkspaceSuccess;
