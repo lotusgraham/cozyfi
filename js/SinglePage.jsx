1d5795b0ef98b958a27a710897dd31bb67513a87
Elias Mason <eliasjmason@gmail.com>
2016-07-25 07:12:48 -0700

    Hotfix: removes assignment of prop in render of Cardz.

--- js/SinglePage.jsx
+++ js/SinglePage.jsx
@@ -49,7 +49,7 @@ const styles = {
 
 const SinglePage = () => (
         <div className = "singlePage" style={styles.container}>
-            <div className="cardz" style={styles.cardz}> <Cardz currentplace ={actions.workspaces.placeId} /> </div>
+            <div className="cardz" style={styles.cardz}> <Cardz/> </div>
             <div className="cozyFiMap" style={styles.cozyfiMap}> <CozyFiMap /> </div>
         </div>
 );
