9e5402f75e8532b2555c0adb5d6125bceef300ee
lotusgraham <gwhitley99@gmail.com>
2016-08-04 13:33:26 -0400

    adds googleData to workspace info

--- js/PlaceInfo.jsx
+++ js/PlaceInfo.jsx
@@ -4,7 +4,7 @@ import {connect} from 'react-redux';
 import * as actions from '../redux/actions/workspace.js';
 import store from '../redux/store.js';
 
-import {Card, CardHeader} from 'material-ui/Card';
+import {Card, CardHeader, CardText} from 'material-ui/Card';
 import update from 'react-addons-update';
 
 
@@ -18,17 +18,17 @@ class PlaceInfo extends React.Component {
         return (
             <div className="zoneinfo">{<Card>
              <CardHeader
-              title={this.props.workspace.googleData}
+              title={this.props.googleData.name}
               subtitle={this.props.workspace.description}
              />
-              <CardText expandable={true}>
+              <CardText>
                   <ul>
-                    <li>{tile.description}</li>
-                    <li>{tile.directions}</li>
-                    <li>{tile.perks}</li>
-                    <li>{tile.quirks}</li>
+                    <li>{this.props.workspace.description}</li>
+                    <li>{this.props.workspace.directions}</li>
+                    <li>{this.props.workspace.perks}</li>
+                    <li>{this.props.workspace.quirks}</li>
                   </ul>
-              </CardText>
+           </CardText>
              </Card>}</div>
         );
     }
@@ -37,8 +37,10 @@ class PlaceInfo extends React.Component {
 
 
 const mapStateToProps = (state, props) => {
+  var workspace = state.workspaceCache[0] || {}
     return {
-        workspace: state.workspaceCache[0] || {}
+        workspace: workspace,
+        googleData: workspace.googleData || {}
     }
 };
 
