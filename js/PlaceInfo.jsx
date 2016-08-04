1df2e5ab195d736a1558457dd8c440d6d56e03f9
lotusgraham <gwhitley99@gmail.com>
2016-08-04 09:58:59 -0400

    fixes issue, pages not building properly due to renaming of build .js files

--- js/PlaceInfo.jsx
+++ js/PlaceInfo.jsx
@@ -8,7 +8,7 @@ export default class PlaceInfo extends React.Component {
 
     render() {
         return (
-    <img src="http://placehold.it/350x800"></img>
+    <img src="http://placehold.it/370x800"></img>
         );
     }
 };
